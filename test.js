b047af8714a11e6a3ab544f7306da05f285f3427
Max GJ Panas <m@maxpanas.com>
2015-11-05 17:59:38 +0200

    add more tests and follow xo rules

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 import test from 'ava';
 import fn from './';
 
+function fizzBuzz(index) {
+	// https://gist.github.com/jaysonrowe/1592432#gistcomment-882551
+	return (++index % 3 ? '' : 'Fizz') + (index % 5 ? '' : 'Buzz') || index;
+}
+
+function indexPlus(index) {
+	return index + 1;
+}
+
 test(t => {
 	t.same(fn('a', 0), []);
 	t.same(fn('a', 1), ['a']);
@@ -8,6 +17,7 @@ test(t => {
 	t.same(fn('a', 5), ['a', 'a', 'a', 'a', 'a']);
 	t.same(fn('foo', 2), ['foo', 'foo']);
 	t.same(fn(0, 2), [0, 0]);
-	t.same(fn(function (index) { return index + 1 }, 3), [1, 2, 3]);
+	t.same(fn(indexPlus, 5), [1, 2, 3, 4, 5]);
+	t.same(fn(fizzBuzz, 15), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
 	t.end();
 });
