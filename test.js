f6260bbd7e89712f04303da31d5902377e4cb868
Max GJ Panas <m@maxpanas.com>
2015-11-05 18:13:42 +0200

    add test for extra item generator params

--- test.js
+++ test.js
@@ -10,6 +10,10 @@ function indexPlus(index) {
 	return index + 1;
 }
 
+function comprehensive(index, length, partial) {
+	return partial.indexOf(index) >= 0 ? length : index + 1;
+}
+
 test(t => {
 	t.same(fn('a', 0), []);
 	t.same(fn('a', 1), ['a']);
@@ -19,5 +23,6 @@ test(t => {
 	t.same(fn(0, 2), [0, 0]);
 	t.same(fn(indexPlus, 5), [1, 2, 3, 4, 5]);
 	t.same(fn(fizzBuzz, 15), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
+	t.same(fn(comprehensive, 5), [1, 5, 3, 5, 5]);
 	t.end();
 });
