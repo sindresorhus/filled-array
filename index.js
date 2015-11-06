6a352448bac77ad7e3ecbab97b6c6c3aac72bc90
Max G J Panas <m@maxpanas.com>
2015-11-06 15:59:06 +0200

    change `isCb` to `isFn`

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 'use strict';
 module.exports = function (item, n) {
 	var ret = new Array(n);
-	var isCb = typeof item === 'function';
+	var isFn = typeof item === 'function';
 
-	if (!isCb && typeof ret.fill === 'function') {
+	if (!isFn && typeof ret.fill === 'function') {
 		return ret.fill(item);
 	}
 
 	for (var i = 0; i < n; i++) {
-		ret[i] = isCb ? item(i, n, ret) : item;
+		ret[i] = isFn ? item(i, n, ret) : item;
 	}
 
 	return ret;
