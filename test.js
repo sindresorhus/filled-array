6b9617d9ab1267a23a07fec47f5de62c81b72fbd
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 13:59:44 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -6,12 +6,11 @@ function indexPlus(index) {
 }
 
 function fizzBuzz(index) {
-	// https://gist.github.com/jaysonrowe/1592432#gistcomment-882551
 	return (++index % 3 ? '' : 'Fizz') + (index % 5 ? '' : 'Buzz') || index;
 }
 
 function comprehensive(index, length, partial) {
-	return partial.includes(index) ? length : index + 1;
+	return partial.indexOf(index) === -1 ? index + 1 : length;
 }
 
 test(t => {
@@ -24,5 +23,4 @@ test(t => {
 	t.same(fn(indexPlus, 5), [1, 2, 3, 4, 5]);
 	t.same(fn(fizzBuzz, 15), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
 	t.same(fn(comprehensive, 5), [1, 5, 3, 5, 5]);
-	t.end();
 });
