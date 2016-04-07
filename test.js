4fe0c43f9681c7eec6c002cc2922098f40904ae9
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 14:11:53 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -14,13 +14,16 @@ function comprehensive(index, length, partial) {
 }
 
 test(t => {
-	t.same(fn('a', 0), []);
-	t.same(fn('a', 1), ['a']);
-	t.same(fn('a', 2), ['a', 'a']);
-	t.same(fn('a', 5), ['a', 'a', 'a', 'a', 'a']);
-	t.same(fn('foo', 2), ['foo', 'foo']);
-	t.same(fn(0, 2), [0, 0]);
-	t.same(fn(indexPlus, 5), [1, 2, 3, 4, 5]);
-	t.same(fn(fizzBuzz, 15), [1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']);
-	t.same(fn(comprehensive, 5), [1, 5, 3, 5, 5]);
+	t.deepEqual(fn('a', 0), []);
+	t.deepEqual(fn('a', 1), ['a']);
+	t.deepEqual(fn('a', 2), ['a', 'a']);
+	t.deepEqual(fn('a', 5), ['a', 'a', 'a', 'a', 'a']);
+	t.deepEqual(fn('foo', 2), ['foo', 'foo']);
+	t.deepEqual(fn(0, 2), [0, 0]);
+	t.deepEqual(fn(indexPlus, 5), [1, 2, 3, 4, 5]);
+	t.deepEqual(
+		fn(fizzBuzz, 15),
+		[1, 2, 'Fizz', 4, 'Buzz', 'Fizz', 7, 8, 'Fizz', 'Buzz', 11, 'Fizz', 13, 14, 'FizzBuzz']
+	);
+	t.deepEqual(fn(comprehensive, 5), [1, 5, 3, 5, 5]);
 });
